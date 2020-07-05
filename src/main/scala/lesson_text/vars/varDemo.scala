00dc837e86cc1d905720d249c3352f4fe16189ce
Zerohclmax <58201626+Zerohclmax@users.noreply.github.com>
2020-07-05 12:11:38 +0800

    Initial commit

---
+++ src/main/scala/lesson_text/vars/varDemo.scala
@@ -0,0 +1,12 @@
+package lesson_text.vars
+
+object varDemo {
+  def main(args: Array[String]): Unit = {
+    var age: Int = 10
+    var sal: Double = 10.9
+    var name: String = "toom"
+    var score: Float = 70.9f
+    var isPass: Boolean = true
+    println(s"${age} ${isPass}")
+  }
+}
